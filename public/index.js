8aaaee866d452ca574ce4fed50685dc78207193b
feel5ny <feel5.nayoung@gmail.com>
2017-09-06 17:11:22 +0900

    second commit

---
+++ public/index.js
@@ -0,0 +1,53 @@
+(function (window, document) {
+
+  /**
+   * 서버에서 할일 템플릿과 할일 데이터를 가져온 후, #todos 요소 안에 렌더링하는 함수
+   */
+  function loadTodos() {
+    console.log('start loadTodos')
+    render({
+      target: '#todos',
+      templatePath: '/templates/todos.ejs',
+      dataPath: '/api/todos'
+    }).then(todosEl => {
+      todosEl.querySelectorAll('.todo-item').forEach(todoItem => {
+        const id = todoItem.dataset.id
+
+        // 체크박스 클릭시
+        // 낙관적 업데이트
+        const checkboxEl = todoItem.querySelector('.todo-checkbox')
+        checkboxEl.addEventListener('click', e => {
+          axios.patch(`/api/todos/${id}`, {
+            complete: e.currentTarget.checked
+          }).then(res => {
+            loadTodos()
+          })
+        })
+
+        // 삭제 아이콘 클릭시
+        // 비관적 업데이트
+        const removeLink = todoItem.querySelector('.todo-remove')
+        removeLink.addEventListener('click', e => {
+          axios.delete(`/api/todos/${id}`).then(res => {
+            loadTodos()
+          })
+        })
+      })
+    })
+  }
+
+  document.querySelector('#todo-form').addEventListener('submit', e => {
+    e.preventDefault()
+    const form = e.currentTarget
+    axios.post('/api/todos', {
+      title: form.elements.title.value
+    })
+      .then(loadTodos)
+      .then(() => {
+        form.elements.title.value = null
+      })
+  })
+
+  loadTodos()
+
+})(window, document)
